1d83e9b985861c1888e5e11cd7c7134b9aa2e774
Abhyudya Singh Ujala <abhyudyasinghujala2002@gmail.com>
2024-09-03 22:47:57 +0530

    .

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import create from "zustand";
 import { ReactFlowProvider } from "reactflow";
-
-// If you're using a custom store
 const useStore = create((set) => ({
   // your state and actions here
 }));
